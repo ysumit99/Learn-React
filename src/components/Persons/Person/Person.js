62292111d07cc67603141d1cda52798cd24c2d17
Sumit Yadav <ysumit99@gmail.com>
2020-03-29 16:53:04 +0530

    Improved Folder Structure

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,13 +1,8 @@
 import React from 'react';
-
 import classes from './Person.css';
 
 const person = (props) => {
-    const rnd = Math.random();
 
-    if (rnd > 0.7) {
-        throw new Error('Something went wrong!');
-    }
     return (
 
         <div className={classes.Person}>
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from './Person/Person';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
+import Person from '../components/Persons/Person/Person';
 
 class App extends Component {
 
@@ -65,7 +64,6 @@ class App extends Component {
 
   render() {
 
-
     let persons = null;
     let btnClass = '';
 
@@ -74,15 +72,14 @@ class App extends Component {
         <div>
           {
             this.state.persons.map((person, index) => {
-              return <ErrorBoundary key={person.id}>
-                <Person
-                  click={() => this.deletePersonHandler(index)}
-                  name={person.name}
-                  age={person.age}
+              return <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={(event) => this.nameChangeHandler(event, person.id)}
+              />
 
-                  changed={(event) => this.nameChangeHandler(event, person.id)}
-                />
-              </ErrorBoundary>
             })
           }
 
