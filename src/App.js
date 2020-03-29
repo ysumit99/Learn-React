e85278a4c913c5e223a8efb5af1416259b0c236f
Sumit Yadav <ysumit99@gmail.com>
2020-03-29 10:52:07 +0530

    Using CSS Modules for scoped styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -64,20 +64,9 @@ class App extends Component {
 
   render() {
 
-    // const style = {
-    //   backgroundColor: 'green',
-    //   font: 'inherit',
-    //   border: '1px solid blue',
-    //   padding: '8px',
-    //   color: 'white',
-    //   cursor: 'pointer',
-    //   ':hover': {
-    //     backgroundColor: 'lightgreen',
-    //     color: 'black'
-    //   }
-    // };
 
     let persons = null;
+    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
@@ -97,29 +86,24 @@ class App extends Component {
         </div>
       );
 
-      // style.backgroundColor = 'red';
-      // style[':hover'] = {
-      //   backgroundColor: 'salmon',
-      //   color: 'black'
-      // }
+      btnClass = classes.Red;
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red'); //classes = ['red']
+      assignedClasses.push(classes.red); //classes = ['red']
     }
 
     if (this.state.persons.length <= 1) {
-      classes.push('bold'); //classes = ['red', 'blue']
+      assignedClasses.push(classes.bold); //classes = ['red', 'blue']
     }
 
     return (
 
-      <div className="App" >
+      <div className={classes.App} >
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <button className="button"
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        <p className={assignedClasses.join(' ')}>This is really working!</p>
+        <button className={btnClass} onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div >
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,34 +1,15 @@
 import React from 'react';
-import styled from 'styled-components';
 
-const StyledDiv = styled.div`
-    width: 60%;
-    margin: 16px auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
-
-    @media (max-width: 500px) {
-        width: 150px;
-    }
-`;
+import classes from './Person.css';
 
 const person = (props) => {
-
-    // const style = {
-    //     '@media (min-width: 500px)': {
-    //         width: '450px'
-    //     }
-    // };
-
     return (
 
-        <StyledDiv>
-            < p onClick={props.click} > I'm a {props.name} and I am {props.age} years old!</p>
-            < p > {props.children}</p >
+        <div className={classes.Person}>
+            <p onClick={props.click} > I'm a {props.name} and I am {props.age} years old!</p>
+            <p> {props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
-        </StyledDiv>
+        </div>
     )
 };
 
